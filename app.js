c3f875e2aa30b57347ca47c19f9eebbb5a1c203f
Anh Nguyen <anh.nguyen@uniphore.com>
2021-03-04 15:17:20 -0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('AWS dive'));
+app.get('/', (req, res) => res.send('AWS Dive'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
